87503f97fc68ce87af7e85d943c91a3d07f50e17
Theo Alves <TheoLvs@users.noreply.huggingface.co>
2023-06-20 13:54:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ system_template = {
 openai.api_type = "azure"
 openai.api_key = os.environ["api_key"]
 openai.api_base = os.environ["ressource_endpoint"]
-openai.api_version = "2022-12-01"
+openai.api_version = "2023-06-01-preview"
 
 retriever = EmbeddingRetriever(
     document_store=FAISSDocumentStore.load(
@@ -195,7 +195,7 @@ def chat(
     #     raise Exception("report_type arg should be in (All available, IPCC only)")
 
     reformulated_query = openai.Completion.create(
-        engine="climateGPT",
+        engine="EkiGPT",
         prompt=get_reformulation_prompt(query),
         temperature=0,
         max_tokens=128,
