3472265488b7de79be59a4f94169c44a6a9b89a8
dorabijvoet <dora.bijvoet@hotmail.fr>
2024-07-11 16:20:04 +0200

    Retrieved recommended graphs

---
+++ climateqa/engine/graph_retriever.py
@@ -0,0 +1,39 @@
+from langchain_core.retrievers import BaseRetriever
+from langchain_core.documents.base import Document
+from langchain_core.vectorstores import VectorStore
+from langchain_core.callbacks.manager import CallbackManagerForRetrieverRun
+
+from typing import List
+
+class GraphRetriever(BaseRetriever):
+    vectorstore:VectorStore
+    sources:list = ["IEA", "OWID"] # plus tard ajouter OurWorldInData # faudra integrate avec l'autre retriever
+    threshold:float = 0.5
+    k_total:int = 10
+
+    def _get_relevant_documents(
+        self, query: str, *, run_manager: CallbackManagerForRetrieverRun
+    ) -> List[Document]:
+
+        # Check if all elements in the list are IEA or OWID
+        assert isinstance(self.sources,list)
+        assert any([x in ["IEA", "OWID"] for x in self.sources])
+
+        # Prepare base search kwargs
+        filters = {}
+
+        filters["source"] = {"$in": self.sources}
+
+        docs = self.vectorstore.similarity_search_with_score(query=query, filter=filters, k=self.k_total)
+        
+        # Filter if scores are below threshold
+        docs = [x for x in docs if x[1] > self.threshold]
+
+        # Add score to metadata
+        results = []
+        for i,(doc,score) in enumerate(docs):
+            doc.metadata["similarity_score"] = score
+            doc.metadata["content"] = doc.page_content
+            results.append(doc)
+
+        return results
